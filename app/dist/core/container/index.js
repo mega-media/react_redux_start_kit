8a4cc6335696a268e86cb448b4d37f6010b6d4db
ArShown <arshown@megalive.com.tw>
2018-03-30 11:41:43 +0800

    增加 PureStore

--- app/dist/core/container/index.js
+++ app/dist/core/container/index.js
@@ -2,7 +2,8 @@
 import Dispatch from './hoc/dispatch';
 import I18n from './hoc/i18n';
 import Store from './hoc/store';
+import { PureStore } from './hoc/store';
 import Style from './hoc/style';
 import { compose } from 'redux';
 
-export { compose, Dispatch, I18n, Store, Style };
+export { compose, Dispatch, I18n, PureStore, Store, Style };
