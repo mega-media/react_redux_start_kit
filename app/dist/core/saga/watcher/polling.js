87871fdf57c359fd40d9e57ab5468e93ac105113
ArShown <arshown@megalive.com.tw>
2018-04-25 19:23:41 +0800

    polling effect 使用 actionChannel 改寫

--- app/dist/core/saga/watcher/polling.js
+++ app/dist/core/saga/watcher/polling.js
@@ -1,10 +1,14 @@
-import { put, take, call, fork, all } from 'redux-saga/effects';
+import { put, take, call, fork, actionChannel } from 'redux-saga/effects';
 import { eventChannel, END } from 'redux-saga';
 import taskManager from '../task-manager';
 
 export function* pollingTask(payload) {
   const channel = yield call(subscribe, payload);
+
   try {
+    /* 先執行一次 */
+    yield put(payload.action);
+    /* 開始 interval 監聽 */
     while (true) {
       let action = yield take(channel);
       yield put(action);
@@ -16,10 +20,10 @@ export function* pollingTask(payload) {
 
 export function subscribe({ action, interval }) {
   return eventChannel(emit => {
-    emit(action);
     let timer = setInterval(() => {
       emit(action);
     }, interval);
+
     return () => {
       clearInterval(timer);
     };
@@ -27,11 +31,16 @@ export function subscribe({ action, interval }) {
 }
 
 export default function*() {
-  while (true) {
-    let action = yield take('SAGA_POLLING');
-    const { payload } = action;
-    const task = yield fork(pollingTask, payload);
-    action.task = task;
-    yield call(taskManager.append, task);
+  const pollingChannel = yield actionChannel('SAGA_POLLING');
+  try {
+    while (true) {
+      let action = yield take(pollingChannel);
+      const { payload } = action;
+      const task = yield fork(pollingTask, payload);
+      action.task = task;
+      yield call(taskManager.append, task);
+    }
+  } finally {
+    pollingChannel.close();
   }
 }
