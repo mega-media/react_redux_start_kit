4e73fad5ba383ef8dfcb7403702eddf47f9a2534
ArShown <arshown@megalive.com.tw>
2018-05-28 15:43:09 +0800

    新增擴充檔案索引別名

--- __test__/effects/index.js
+++ __test__/effects/index.js
@@ -2,7 +2,7 @@ export { default as call } from './call';
 export { default as cancel } from './cancel';
 export { default as cancelLeast } from './cancel-latest';
 export { default as delay } from './delay';
-export { default as fetchAPI } from './fetch-api';
+export { default as fetchApi } from './fetch-api';
 export { default as lock } from './lock';
 export { default as unlock } from './unlock';
 export { default as polling } from './polling';
