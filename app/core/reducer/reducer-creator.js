e29517ebaf7f9e3c8b620c80f186f911bb8bfba3
ArShown <ArShown@users.noreply.github.com>
2020-10-16 09:27:15 +0800

    add: 引入型別檢查 (#49)
    - 改寫 reducer-creator
    - 增加 state 格式檢查確保不會發生因型別錯誤而導致的無法預期錯誤

--- app/core/reducer/reducer-creator.js
+++ app/core/reducer/reducer-creator.js
@@ -1,8 +1,19 @@
-import { has } from 'ramda';
+import SchemaValidator from '@mega-media/schema-validator';
+import { identity, has } from 'ramda';
 
-const reducerCreator = (initState, actionMaps) => (
-  state = initState,
-  { type, payload }
-) => (has(type)(actionMaps) ? actionMaps[type](state, payload) : state);
+const reducerCreator = (initState, actionMaps, schema = null) => {
+  let validate = identity;
+  if (schema !== null) {
+    const validator = new SchemaValidator(initState);
+    validator.errorCallback = msg => {
+      console.error(msg);
+    };
+    validator.schema = schema;
+    validate = validator.update;
+  }
+
+  return (state = validate(initState), { type, payload }) =>
+    has(type)(actionMaps) ? validate(actionMaps[type](state, payload)) : state;
+};
 
 export default reducerCreator;
--- app/src/storage/reducers/observable.js
+++ app/src/storage/reducers/observable.js
@@ -2,13 +2,25 @@ import { reducerCreator } from '@core/reducer';
 
 const defaultState = { isPinging: false };
 
-const reducer = reducerCreator(defaultState, {
-  PING: () => ({
-    isPinging: true
-  }),
-  PONG: () => ({
-    isPinging: false
-  })
-});
+const reducer = reducerCreator(
+  defaultState,
+  {
+    PING: () => ({
+      isPinging: true
+    }),
+    PONG: () => ({
+      isPinging: false
+    })
+  },
+  {
+    type: 'object',
+    properties: {
+      isPinging: {
+        type: 'boolean'
+      }
+    },
+    additionalProperties: false
+  }
+);
 
 export default reducer;
